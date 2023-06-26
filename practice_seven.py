b12ad765d9d6a9a3ee96067b173780a9d526f230
Kiran Bansal <136809550+Kiran-bansal@users.noreply.github.com>
2023-06-26 10:23:31 +0530

    Create practice_seven.py

---
+++ practice_seven.py
@@ -0,0 +1,11 @@
+money=int(input("Enter your money:\n"))
+if money>70000:
+    print("You purchase iphone")
+elif money>60000 and money<70000:
+    print("You purchase vivo")
+elif money>50000 and money<60000:
+    print("You purchase samsung")
+elif money>40000 and money<50000:
+    print("You purchase oneplus")
+elif money>10000 and money<40000:
+    print("You purchase redmi")
