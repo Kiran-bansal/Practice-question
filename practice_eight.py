33ba622577ba597f5a09076fcf52c9bc39c76a31
Kiran Bansal <136809550+Kiran-bansal@users.noreply.github.com>
2023-06-26 10:24:12 +0530

    Create practice_eight.py

---
+++ practice_eight.py
@@ -0,0 +1,15 @@
+day=input("Enter the day:\n")
+if day=="mon":
+    print("play football")
+elif day=="tue":
+    print("play cricket")
+elif day=="wed":
+    print("play hockey")
+elif day=="thur":
+    print("play ludo")
+elif day=="fri":
+    print("play TT")
+elif day=="sat":
+    print("watch movie")
+elif day=="sun":
+    print("enjoy")
